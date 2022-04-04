4ddf7179f27bdf18922d361c7702af6b233703f7
Kamden Bolhuis <kamden.bolhuis@gmail.com>
2022-04-04 13:02:08 -0400

    Add bookstore project

---
+++ Bookstore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bookstore
+{
+    class Program
+    {
+        /**
+            * Search for a book by author or namespace
+            * Add a book to the collection 
+            * Delete a book from the collection
+            * Print all books in the collection
+            * https://www.kaggle.com/datasets/saurabhbagchi/books-dataset
+        **/
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
