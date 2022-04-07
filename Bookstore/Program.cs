f3919a6b68718aa2caed8536c852a149d5cbf4d0
Kamden Bolhui <kamden@kamdenbolhuis.com>
2022-04-07 10:53:11 -0400

    Working on bookstore methods

--- Bookstore/Program.cs
+++ Bookstore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 using System.Globalization;
 
@@ -7,7 +8,7 @@ using CsvHelper;
 
 namespace Bookstore
 {
-    class Program
+    class Bookstore
     {
         /**
             * Search for a book by author or namespace
@@ -18,14 +19,14 @@ namespace Bookstore
                 * Had to replcae all \" with '
         **/
 
-        record Book(string ISBN, string BookTitle, string Author, double YearOfPublication, string Publisher);
+
+        public static IEnumerable<Book> books;
+    
+        public record Book(string ISBN, string BookTitle, string Author, double YearOfPublication, string Publisher);
        
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            using var streamReader = File.OpenText("books.csv");
-            using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
-            IEnumerable<Book> books;
-            books = csvReader.GetRecords<Book>();
+            openFile("books.csv");
 
             bool running = true;
 
@@ -35,23 +36,30 @@ namespace Bookstore
                 Console.WriteLine("2. Add a book to the collection");
                 Console.WriteLine("3. Delete a book from the collection");
                 Console.WriteLine("4. Exit");
+                Console.WriteLine("5. Save");
 
                 string input = Console.ReadLine();
 
                 switch(input){
                     case "1":
-                        Console.WriteLine("Please enter the author or name of the book you are looking for:");
+                        Console.WriteLine("Please enter the name of the book you are looking for:");
                         string bookSearch = Console.ReadLine();
-                        search(books, bookSearch);
+                        search(bookSearch);
                         break;
                     case "2":
                         // add();
                         break;
                     case "3":
-                        // delete(books, input);
+                        Console.WriteLine("Please enter the ISBN of a book you would like to delete or q to cancel");
+                        string bookDelete = Console.ReadLine();
+                        if(bookDelete == "q") break;
+                        // delete(ref books, bookDelete);
                         break;
                     case "4":
                         running = false;
+                        break;
+                    case "5":
+                        // saveFile(books, "books.csv");
                         break;
                     default:
                         Console.WriteLine("Invalid input");
@@ -61,12 +69,19 @@ namespace Bookstore
             // search(books, "More Than a Dream");
         }
 
-        static void search(IEnumerable<Book> books, string searchTerm)
+        public static void openFile(string file)
+        {
+            using var streamReader = File.OpenText(file);
+            using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            books = csvReader.GetRecords<Book>();
+        }
+
+        public static void search(string searchTerm)
         {
             int found = 0;
-            foreach(var book in books) /** O(n) **/
+            foreach(var book in books) /** O(n) time and space complexity **/
             {
-                if(book.BookTitle.Contains(searchTerm) || book.Author.Contains(searchTerm))
+                if(book.BookTitle.ToLower().Contains(searchTerm.ToLower()))
                 {
                     found++;
                     Console.WriteLine("\nISBN {0}: {1} by {2} \n", book.ISBN, book.BookTitle, book.Author);
@@ -75,14 +90,27 @@ namespace Bookstore
             Console.WriteLine("Found {0} books", found);
         }
 
-        // static void add(IEnumerable<Book> books, Book book)
+        // static void add(ref IEnumerable<Book> books, Book book)
         // {
         //     books.Add(book);
         // }
 
-        // static void delete(ref IEnumerable<Book> books, string ISBN)
+        // public void delete(ref IEnumerable<Book> books, string ISBN)
         // {
-        //    books = books.Where(book => book.ISBN != ISBN);
+        //     try {
+        //         books = books.Where(book => book.ISBN != ISBN);
+        //         Console.WriteLine("Successfully removed {0}", ISBN);
+        //     }
+        //     catch(Exception e) {
+        //         Console.WriteLine("Error: {0}", e.Message);
+        //     }
+        // }
+
+        // public void saveFile(IEnumerable<Book> books, string file){
+        //     using var streamWriter = File.CreateText(file);
+        //     using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+        //     csvWriter.WriteRecords(books);
+        //     openFile();
         // }
     }
 }
---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+
+
+            someMethod();
+        }
+
+        public void someMethod(){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
