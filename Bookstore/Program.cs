8fb1a9c9aab815a4ed6931201d449a57627d2418
Kamden Bolhui <kamden@kamdenbolhuis.com>
2022-04-14 00:29:33 -0400

    Bookstore work

--- Bookstore/Program.cs
+++ Bookstore/Program.cs
@@ -8,6 +8,15 @@ using CsvHelper;
 
 namespace Bookstore
 {
+
+    class Book {
+        public string ISBN { get; set;}
+
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public string YearOfPublication { get; set; }
+        public string Publisher { get; set; }
+    }
     class Bookstore
     {
         /**
@@ -19,30 +28,35 @@ namespace Bookstore
                 * Had to replcae all \" with '
         **/
 
+        /**
+            TODO:
+                - Add a book to the collection
+                - Delete a book from the collection
+
+        **/
+
 
         public static IEnumerable<Book> books;
-    
-        public record Book(string ISBN, string BookTitle, string Author, double YearOfPublication, string Publisher);
-       
+        public static IEnumerable<Book> testBooks;
         public static void Main(string[] args)
         {
-            openFile("books.csv");
+            openFile("testbooks.csv");
 
             bool running = true;
 
             while(running){
-                Console.WriteLine("Please choose an option:");
-                Console.WriteLine("1. Search for a book by author or name");
+                Console.WriteLine("1. Search for a book by author or name; {0} books available", books.Count());
                 Console.WriteLine("2. Add a book to the collection");
                 Console.WriteLine("3. Delete a book from the collection");
-                Console.WriteLine("4. Exit");
-                Console.WriteLine("5. Save");
+                Console.WriteLine("4. Save file override");
+                Console.WriteLine("5. Exit");
+                Console.Write("Please choose an option: ");
 
                 string input = Console.ReadLine();
 
                 switch(input){
                     case "1":
-                        Console.WriteLine("Please enter the name of the book you are looking for:");
+                        Console.Write("Please enter the name of the book you are looking for: ");
                         string bookSearch = Console.ReadLine();
                         search(bookSearch);
                         break;
@@ -50,67 +64,102 @@ namespace Bookstore
                         // add();
                         break;
                     case "3":
-                        Console.WriteLine("Please enter the ISBN of a book you would like to delete or q to cancel");
+                        Console.Write("\nPlease enter the ISBN of a book you would like to delete or q to cancel: ");
                         string bookDelete = Console.ReadLine();
                         if(bookDelete == "q") break;
-                        // delete(ref books, bookDelete);
+                        delete(ref books, bookDelete);
                         break;
                     case "4":
-                        running = false;
+                        saveFile(books, "testbooks.csv");
                         break;
                     case "5":
-                        // saveFile(books, "books.csv");
+                        running = false;
                         break;
                     default:
                         Console.WriteLine("Invalid input");
                         break;
                 }
             }
-            // search(books, "More Than a Dream");
         }
 
         public static void openFile(string file)
         {
-            using var streamReader = File.OpenText(file);
-            using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
-            books = csvReader.GetRecords<Book>();
+            try 
+            {
+                using var streamReader = File.OpenText(file);
+                using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+                books = csvReader.GetRecords<Book>().ToList();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
 
         public static void search(string searchTerm)
         {
+            Console.WriteLine("\nSearching");
             int found = 0;
             foreach(var book in books) /** O(n) time and space complexity **/
             {
-                if(book.BookTitle.ToLower().Contains(searchTerm.ToLower()))
+                if(cleanString(book.BookTitle).Contains(cleanString(searchTerm)))
                 {
                     found++;
-                    Console.WriteLine("\nISBN {0}: {1} by {2} \n", book.ISBN, book.BookTitle, book.Author);
+                    Console.WriteLine("ISBN {0}: {1} by {2}", book.ISBN, book.BookTitle, book.Author);
                 }
             }
-            Console.WriteLine("Found {0} books", found);
+            Console.WriteLine("Found {0} books\n", found);
         }
 
-        // static void add(ref IEnumerable<Book> books, Book book)
-        // {
-        //     books.Add(book);
-        // }
 
-        // public void delete(ref IEnumerable<Book> books, string ISBN)
-        // {
-        //     try {
-        //         books = books.Where(book => book.ISBN != ISBN);
-        //         Console.WriteLine("Successfully removed {0}", ISBN);
-        //     }
-        //     catch(Exception e) {
-        //         Console.WriteLine("Error: {0}", e.Message);
-        //     }
-        // }
+        /** 
+            Clean string to help find books without being exactly right
+                - Remove [ ' ]
+                - Remove [ - ]
+                - Remove [ , ]
+                - To lower case
+        **/
+        public static string cleanString(string input)
+        {
 
-        // public void saveFile(IEnumerable<Book> books, string file){
-        //     using var streamWriter = File.CreateText(file);
-        //     using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
-        //     csvWriter.WriteRecords(books);
-        //     openFile();
-        // }
+            return 
+                input.Replace("-", " ") // replace - with space
+                .Replace(",", "") // remove ,
+                .Replace("\"", "'") // remove " and repalce '
+                .ToLower();
+        }
+
+        public static void delete(ref IEnumerable<Book> books, string ISBN)
+        {
+            try {
+                books = books.Where(book => book.ISBN != ISBN);
+                Console.WriteLine("Successfully removed {0}\n", ISBN);
+                saveFile(books, "books.csv");
+            }
+            catch(Exception e) {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+        /**
+            TODO:
+                - Fix it not putting strings around the output
+                    , , , ->  "","",""
+        **/
+        public static void saveFile(IEnumerable<Book> books, string file)
+        {
+            try{
+                using var streamWriter = File.CreateText(file);
+                using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+                csvWriter.WriteRecords(books);
+                openFile(file);
+                Console.WriteLine("Successfully saved");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
--- test/Program.cs
+++ test/Program.cs
@@ -1,21 +1,171 @@
 ﻿using System;
+using static System.Console;
+using System.IO;
+using System.Collections.Generic;
 
-namespace test
+
+namespace SampleChapter14Demo
 {
-    class Program
+    class StudentApp
     {
-        public static void Main(string[] args)
+        const char DELIM = ',';
+        const string END = "end";
+        const string FILENAME = "Students.txt";
+        public static List<Student> stuList = new List<Student> ();
+
+        static void Main()
         {
 
 
+            //load file, fix this line
+            if (!loadFile(FILENAME))
+            {
+                WriteLine("File Loaded Successfully");
+
+                Student stu = new Student();
+                
+                Write("Add Student number or '" + END + "' to quit ");
+                stu.StuNum = ReadLine();
+                while (stu.StuNum != END)
+                {
+                    Write("Enter last name ");
+                    stu.LName = ReadLine();
+                    Write("Enter first name ");
+                    stu.FName = ReadLine();
+                    Write("Enter gpa ");
+                    stu.Gpa = Convert.ToDouble(ReadLine());
+                    // what do I need to do next? add one line of code.
+                    stuList.Add(stu);
+                    //
+
+                    Write("Enter Student number or " + END + " to quit ");
+                    stu.StuNum = ReadLine();
+                }
+
+                if (saveFile(FILENAME) == false)
+                {
+                    WriteLine("File Saved successfully.");
+                } else
+                {
+                    WriteLine("File Save Error");
+                }
 
 
 
-            someMethod();
+            } else { 
+                WriteLine("File Load Error");
+            }
+
         }
 
-        public void someMethod(){
-            Console.WriteLine("Hello World!");
+        private static bool saveFile(string outFileName)
+        {
+            bool error = false;
+
+            try
+            {
+                FileStream outFile = new FileStream(FILENAME, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter writer = new StreamWriter(outFile);
+
+                // different construct used to loop through List
+                //
+                foreach (Student curStu in stuList)
+                {
+                    string outRec = curStu.StuNum;
+                    outRec += "," + curStu.LName;
+                    outRec += "," + curStu.FName;
+                    outRec += "," + curStu.Gpa.ToString();
+
+                    writer.WriteLineAsync(outRec);
+                }
+                outFile.Close();
+            }
+            catch
+            {
+                error = true;
+            }
+            return error;
+        }
+
+        public static bool loadFile(string inFileName)
+        {            
+            WriteLine("\n{0,-5}{1,-12}{2,8}\n", "Num", "Name", "GPA");
+
+            bool error = false;
+
+            try
+            {
+                FileStream inFile = new FileStream(inFileName, FileMode.OpenOrCreate, FileAccess.Read);
+                StreamReader reader = new StreamReader(inFile);
+
+                string recordIn;
+                string[] fields;
+                recordIn = reader.ReadLine();
+
+                
+                while (recordIn != null)
+                {
+                    // get the record pulled in
+                    fields = recordIn.Split(DELIM);
+                    
+
+                    Student stu = new Student();
+                    // how to parse out 
+                    stu.StuNum = fields[0];
+                    stu.FName = fields[2];
+                    stu.LName = fields[1];
+                    stu.Gpa = double.Parse(fields[3]);
+                    stuList.Add(stu);
+
+
+
+                    WriteLine("{0,-5}{1,-12}{2,8}", stu.StuNum, stu.LName, stu.FName, stu.Gpa.ToString("F2"));
+
+                    recordIn = reader.ReadLine();
+
+                }
+                inFile.Close();
+            } catch
+            {
+                error = true;
+            }
+            return error;
+        }
+
+    }
+    public class Student
+    {
+        private string stuNum;
+        private string lname;
+        private string fname;
+        private double gpa;
+        private const double MINGPA = 0.0;
+        private const double MAXGPA = 4.0;
+        public string StuNum
+        {
+            get { return stuNum; }
+            set { stuNum = value; }
+        }
+        public string LName
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public string FName
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+        public double Gpa
+        {
+            get { return gpa; }
+            set
+            {
+                if (value >= MINGPA && value <= MAXGPA)
+                    gpa = value;
+                else
+                    gpa = 0;
+            }
         }
     }
 }
