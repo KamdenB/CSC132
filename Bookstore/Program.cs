3959ca53fc0d596e55fdd9ac1686e5a18dc1266e
Kamden Bolhui <kamden@kamdenbolhuis.com>
2022-04-14 12:25:45 -0400

    Bookstore finished, working, and submited for grading

--- Bookstore/Program.cs
+++ Bookstore/Program.cs
@@ -37,19 +37,17 @@ namespace Bookstore
 
 
         public static IEnumerable<Book> books;
-        public static IEnumerable<Book> testBooks;
         public static void Main(string[] args)
         {
-            openFile("testbooks.csv");
+            openFile("Bookstore.csv");
 
             bool running = true;
 
             while(running){
-                Console.WriteLine("1. Search for a book by author or name; {0} books available", books.Count());
+                Console.WriteLine("1. Search for a book by title; {0} books available", books.Count());
                 Console.WriteLine("2. Add a book to the collection");
                 Console.WriteLine("3. Delete a book from the collection");
-                Console.WriteLine("4. Save file override");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("4. Exit");
                 Console.Write("Please choose an option: ");
 
                 string input = Console.ReadLine();
@@ -61,7 +59,7 @@ namespace Bookstore
                         search(bookSearch);
                         break;
                     case "2":
-                        // add();
+                        add();
                         break;
                     case "3":
                         Console.Write("\nPlease enter the ISBN of a book you would like to delete or q to cancel: ");
@@ -70,9 +68,6 @@ namespace Bookstore
                         delete(ref books, bookDelete);
                         break;
                     case "4":
-                        saveFile(books, "testbooks.csv");
-                        break;
-                    case "5":
                         running = false;
                         break;
                     default:
@@ -99,8 +94,9 @@ namespace Bookstore
 
         public static void search(string searchTerm)
         {
-            Console.WriteLine("\nSearching");
+            Console.WriteLine("\nSearching...");
             int found = 0;
+
             foreach(var book in books) /** O(n) time and space complexity **/
             {
                 if(cleanString(book.BookTitle).Contains(cleanString(searchTerm)))
@@ -113,8 +109,44 @@ namespace Bookstore
         }
 
 
+        public static void add()
+        {
+            Console.WriteLine("\nAdding a book...");
+            Console.Write("Please enter the ISBN: ");
+            string isbn = Console.ReadLine();
+            Console.Write("Please enter the title: ");
+            string title = Console.ReadLine();
+            Console.Write("Please enter the author: ");
+            string author = Console.ReadLine();
+            Console.Write("Please enter the year of publication: ");
+            string year = Console.ReadLine();
+            Console.Write("Please enter the publisher: ");
+            string publisher = Console.ReadLine();
+
+
+            try{
+
+                Book newBok = new Book(){
+                    ISBN = isbn,
+                    BookTitle = title,
+                    Author = author,
+                    YearOfPublication = year,
+                    Publisher = publisher
+                };
+
+                books = books.Concat(new List<Book>(){newBok});
+                saveFile(books, "Bookstore.csv");
+                Console.WriteLine("Book added");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
         /** 
-            Clean string to help find books without being exactly right
+            Clean string to help filter out annoying minor differences
                 - Remove [ ' ]
                 - Remove [ - ]
                 - Remove [ , ]
@@ -130,30 +162,24 @@ namespace Bookstore
                 .ToLower();
         }
 
-        public static void delete(ref IEnumerable<Book> books, string ISBN)
+        public static void delete(ref IEnumerable<Book> bookList, string ISBN)
         {
             try {
-                books = books.Where(book => book.ISBN != ISBN);
+                bookList = bookList.Where(book => book.ISBN != ISBN); // Remove books from current list of books
+                saveFile(books, "Bookstore.csv"); // Save new list of books
                 Console.WriteLine("Successfully removed {0}\n", ISBN);
-                saveFile(books, "books.csv");
             }
             catch(Exception e) {
                 Console.WriteLine("Error: {0}", e.Message);
             }
         }
 
-        /**
-            TODO:
-                - Fix it not putting strings around the output
-                    , , , ->  "","",""
-        **/
         public static void saveFile(IEnumerable<Book> books, string file)
         {
             try{
                 using var streamWriter = File.CreateText(file);
                 using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
                 csvWriter.WriteRecords(books);
-                openFile(file);
                 Console.WriteLine("Successfully saved");
             }
             catch(Exception e)
